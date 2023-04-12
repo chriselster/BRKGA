7adcac690695874f49f7c20200d1ecebc26e0896
Christian <christian.elster0@gmail.com>
2023-04-11 23:36:15 -0300

    fix build

--- src/Entities/PathOptimizer.cpp
+++ src/Entities/PathOptimizer.cpp
@@ -1,36 +1,46 @@
 #include "PathOptimizer.hpp"
 
-PathOptimizer::PathOptimizer(std::vector<Point *>){
-    for(int i = 0; i < points.size(); i++){
+PathOptimizer::PathOptimizer(std::vector<Point *>)
+{
+    for (int i = 0; i < points.size(); i++)
+    {
         addPoint(points[i]);
     }
 }
 
-double PathOptimizer::getShortestPath(){
+double PathOptimizer::getShortestPath()
+{
     std::vector<int> path;
-    for(int i = 0; i < points.size(); i++){
+    for (int i = 0; i < points.size(); i++)
+    {
         path.push_back(i);
     }
     double shortestPath = 0;
-    do{
+    do
+    {
         double pathDistance = 0;
-        for(int i = 0; i < path.size() - 1; i++){
+        for (int i = 0; i < path.size() - 1; i++)
+        {
             pathDistance += distanceMatrix[path[i]][path[i + 1]];
         }
-        if(shortestPath == 0 || pathDistance < shortestPath){
+        if (shortestPath == 0 || pathDistance < shortestPath)
+        {
             shortestPath = pathDistance;
         }
-    }while(std::next_permutation(path.begin(), path.end()));
+    } while (std::next_permutation(path.begin(), path.end()));
     return shortestPath;
 }
-void PathOptimizer::addPoint(Point *){
+void PathOptimizer::addPoint(Point *point)
+{
     points.push_back(point);
     distanceMatrix.resize(points.size());
-    for(int i = 0; i < points.size(); i++){
+    for (int i = 0; i < points.size(); i++)
+    {
         distanceMatrix[i].resize(points.size());
     }
     int lastPointIndex = points.size() - 1;
-    for(int i = 0; i < points.size(); i++){
+    for (int i = 0; i < points.size(); i++)
+    {
         double distance = points[i]->distanceTo(point);
         distanceMatrix[i][lastPointIndex] = distance;
         distanceMatrix[lastPointIndex][i] = distance;
--- src/Headers/PathOptimizer.hpp
+++ src/Headers/PathOptimizer.hpp
@@ -1,10 +1,15 @@
+#include <vector>
+#include <algorithm>
+#include "Point.hpp"
 
-class PathOptimizer{
-    private:
+class PathOptimizer
+{
+private:
     std::vector<Point *> points;
     std::vector<std::vector<double>> distanceMatrix;
-    public:
+
+public:
     PathOptimizer(std::vector<Point *>);
     double getShortestPath();
     void addPoint(Point *);
-}
+};
