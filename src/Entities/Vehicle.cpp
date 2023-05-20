cfa3c0c29e8a42c655c35e1eaf168f0edcf9f157
Christian <christian.elster0@gmail.com>
2023-05-20 13:07:02 -0300

    fix price

--- src/Entities/Vehicle.cpp
+++ src/Entities/Vehicle.cpp
@@ -107,7 +107,7 @@ double Vehicle::calculateTripCost(Item *item)
 {
     double cost = 0;
     if (alreadyVisited(item->clientId))
-        return 0; // aumentar o preço com o aumento de peso
+        return costPerKmPerWeight * item->weight;
     if (visitedClients.size() > 0)
         cost += additionalForMultipleClients;
     cost += calculateDeadFreightCost(item);
